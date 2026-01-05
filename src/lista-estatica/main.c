03e3048b82f4ce15dc3187d35c320efd7d132f1a
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2026-01-05 20:04:33 -0300

    feat: implementado a parte mais facil

--- src/lista-estatica/main.c
+++ src/lista-estatica/main.c
@@ -1,2 +1,147 @@
 #include "main.h"
 #include "stdio.h"
+#include <stdlib.h>
+
+// devolve 0 caso seja erro
+int _isExist(List *li) {
+  return (li != NULL);
+}
+
+List *create() {
+  List *li = (List *) malloc(sizeof(List));
+  li->qtd = 0;
+  return li;
+}
+
+void dell(List *li) {
+  free(li);
+}
+
+int isNull(List *li) {
+  if (!_isExist(li))
+    return 0;
+
+  return (li->qtd == 0);
+}
+
+int isFull(List *li) {
+  if (!_isExist(li))
+    return 0;
+
+  return (li->qtd == MAX);
+}
+
+int size(List *li) {
+  if (!_isExist(li))
+    return 0;
+
+  return li->qtd;
+}
+
+int insert_final(List *li, int dado) {
+  if (!_isExist(li) || isFull(li))
+    return 0;
+
+  li->dado[li->qtd] = dado;
+  li->qtd++;
+  return 1;
+}
+
+int insert_begin(List* li, int dado){
+  if (!_isExist(li) || isFull(li))
+    return 0;
+
+  for (int i = (li->qtd - 1); i >= 0; i--) {
+    li->dado[i+1] = li->dado[i];
+  }
+
+  li->dado[0] = dado;
+  li->qtd++;
+  return 1;
+}
+
+int insert_middle(List* li, int dado, int position){
+  if (!_isExist(li) || isFull(li))
+    return 0;
+
+  if (position == 0) {
+    insert_begin(li, dado);
+  }
+
+  if (position >= li->qtd) {
+    insert_final(li, dado);
+  }
+
+  for (int i = (li->qtd - 1); i >= position; i--) {
+    li->dado[i+1] = li->dado[i];
+  }
+
+  li->dado[position] = dado;
+  li->qtd++;
+  return 1;
+}
+
+int remove_final(List* li){
+  if (!_isExist(li) || isNull(li))
+    return 0;
+
+  li->qtd--;
+  return 1;
+}
+
+int remove_begin(List* li){
+  if (!_isExist(li) || isNull(li))
+    return 0;
+
+  for (int i = 0; i != li->qtd; i++) {
+    li->dado[i] = li->dado[i+1];
+  }
+
+  li->qtd--;
+
+  return 1;
+}
+
+int remove_middle(List *li, int position) {
+  if (!_isExist(li) || isNull(li))
+    return 0;
+
+  if (position == 0) {
+    remove_begin(li);
+  }
+
+  if (position >= li->qtd) {
+    remove_final(li);
+  }
+
+
+  for (int i = 0; i != position; i++) {
+    li->dado[i] = li->dado[i+1];
+  }
+
+  li->qtd--;
+
+  return 1;
+  
+}
+
+int remove_item(List *li, int dado) {
+  if (!_isExist(li) || isNull(li))
+    return 0;
+  int position = 0;
+
+  for (int i = 0; i != li->qtd; i++) {
+    if (li->dado[i] == dado) {
+      position = i;
+    } 
+  }
+
+  for (int i = position; i < li->qtd; i++) {
+    li->dado[i] = li->dado[i+1];
+  }
+
+  li->qtd--;
+
+  return 1;
+  
+}
--- src/lista-estatica/main.h
+++ src/lista-estatica/main.h
@@ -1,6 +1,26 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#define MAX 100
 
+typedef struct list List;
+
+struct list {
+  int qtd;
+  int dado[MAX];
+};
+
+List* create();
+void dell(List* li);
+int isNull(List* li);
+int isFull(List* li);
+int size(List* li);
+int insert_final(List* li, int dado);
+int insert_begin(List* li, int dado);
+int insert_middle(List* li, int dado, int position);
+int remove_final(List* li);
+int remove_begin(List* li);
+int remove_item(List* li, int dado);
+int remove_middle(List* li, int position);
 
 #endif // MAIN_H_
