37c17fefe391413892e252b656aa3601ea9bd911
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2026-02-12 14:56:34 -0300

    feat: fazendo as duas pilhas

---
+++ src/pilha-estatica/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+typedef struct Pilha* stack;
+/* struct Pilha { */
+/*   int total; */
+/*   int dados[MAX]; */
+/* }; */
+
+stack* create();
+void dell(stack *pilha);
+
+int isFull(stack *pilha);
+int isNull(stack *pilha);
+int length(stack *pilha);
+
+int Pop(stack *pilha);
+int Push(stack *pilha, int dado);
+
+
+#endif // MAIN_H_
